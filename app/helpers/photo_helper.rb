40c6129036634b0358ced894078951617e177bce
Marc Vitalis <marc.vitalis@live.com>
2010-06-05 07:49:21 +0800

    file cleanup

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -1,4 +1,7 @@
 module PhotoHelper
-  GALLERY_HOST = Flickr.new("#{RAILS_ROOT}/config/flickr.yml")
-  GALLERY = GALLERY_HOST.photos.search(:user_id => APP_CONFIG["flickr"]["user_id"])
+  @photo_host ||= Flickr.new("#{RAILS_ROOT}/config/flickr.yml")
+  
+  def photo_gallery
+    
+  end
 end
