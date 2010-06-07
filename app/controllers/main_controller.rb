9579de674e2d6fd2196ee2e10c873242ad0acead
Marc Vitalis <marc.vitalis@live.com>
2010-06-08 01:19:00 +0800

    added admin for refresh

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,4 +4,9 @@ class MainController < ApplicationController
 
   def about
   end
+  
+  def refresh
+    Gallery.refresh
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "main", :action => "index"
   map.about "/about", :controller => "main", :action => "about"
+  map.refresh "/refresh", :controller => "main", :action => "refresh"
 
   # See how all your routes lay out with "rake routes"
 
