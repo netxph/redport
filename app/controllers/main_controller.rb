6fc9dc358178399446b097ae282a91a2b7fd1ba1
Marc Vitalis <marc.vitalis@live.com>
2010-06-17 19:21:18 +0800

    added featured template

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
+    
   end
 
   def about
--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -1,5 +1,5 @@
 module GalleryHelper
   def render_photo(photo)
-    "<a href=\"#{photo.url(:large)}\" rel=\"lightbox[recent]\"><img src=\"#{photo.url(:medium)}\" /></a>"
+    "<a href=\"#{photo.url(:large)}\" rel=\"lightbox[recent]\" title=\"#{photo.title}\"><img src=\"#{photo.url(:medium)}\" /></a>"
   end
 end
