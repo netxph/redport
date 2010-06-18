856cdcc635831a263c7454b335434728312457a2
Marc Vitalis <marc.vitalis@live.com>
2010-06-18 08:55:22 +0800

    v1.1.1 release candidate

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,8 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'mime-types', :lib => 'mime/types'
-  config.gem 'flickr-fu', :lib => 'flickr_fu'
+  config.gem 'mime-types', :lib => 'mime/types', :version => '1.16'
+  config.gem 'flickr-fu', :lib => 'flickr_fu', :version => '0.1.4'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
