84ef6ceebb4f6a9d5025fad357db7f642f34276a
Marc Vitalis <marc.vitalis@live.com>
2010-06-20 08:11:36 +0800

    v1.1.2 Final Release

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    featured_index = 1 + rand(Gallery.photos.count)
+    featured_index = 1 + rand(Gallery.photos.total)
     @featured_photo = Gallery.photos[featured_index]
 
     #just in case it returns null
