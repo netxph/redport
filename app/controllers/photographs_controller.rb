7e57d0a7fda2a03a06cf0c7ef756f696cd29f298
Marc Vitalis <marc.vitalis@live.com>
2010-09-20 09:10:11 +0800

    added page loading animation

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -2,12 +2,19 @@ class PhotographsController < ApplicationController
   before_filter :authorize
 
   def import
+  end
+
+  def remote_import
     @title = "Import pictures from external repository."
 
     if Photograph.import_from_web 
       flash[:notice] = "Sucessfully imported pictures."
     else
       flash[:error] = "Error importing pictures. Please try again."
+    end
+
+    render :update do |page|
+      page.replace_html "display_ajax", :partial => 'remote_import'
     end
   end
 
