d28c5721e4ad047447f089b5f7d091f58b4aacf5
Marc Vitalis <marc.vitalis@live.com>
2010-09-21 09:13:24 +0800

    implemented ajax preload on admin operations

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -2,29 +2,34 @@ class PhotographsController < ApplicationController
   before_filter :authorize
 
   def import
+    @title = "Import pictures from external repository."
   end
 
   def remote_import
-    @title = "Import pictures from external repository."
-
     if Photograph.import_from_web 
-      flash[:notice] = "Sucessfully imported pictures."
+      render :update do |page|
+        page.replace_html "message", :partial => "shared/remote_success"
+      end
     else
-      flash[:error] = "Error importing pictures. Please try again."
-    end
-
-    render :update do |page|
-      page.replace_html "display_ajax", :partial => 'remote_import'
+      render :update do |page|
+        page.replace_html "message", :partial => "shared/remote_failed"
+      end
     end
   end
 
   def refresh
     @title = "Generate thumbnails and seadragon slices."
+  end
 
+  def remote_refresh
     if Gallery.refresh(Photograph.all_cached)
-      flash[:notice] = "Files successfully generated."
+      render :update do |page|
+        page.replace_html "message", :partial => "shared/remote_success"
+      end
     else
-      flash[:error] = "Error generating files. Please try again."
+      render :update do |page|
+        page.replace_html "message", :partial => "shared/remote_failed"
+      end
     end
   end
 
