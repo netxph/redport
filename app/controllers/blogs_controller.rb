7a0ee19dc7d10f03ddeed44bab00a77947bd48ea
Marc Vitalis <marc.vitalis@live.com>
2010-09-23 08:58:30 +0800

    added news/blog scaffold

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,83 @@
+class BlogsController < ApplicationController
+  # GET /blogs
+  # GET /blogs.xml
+  def index
+    @blogs = Blog.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @blogs }
+    end
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.xml
+  def show
+    @blog = Blog.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @blog }
+    end
+  end
+
+  # GET /blogs/new
+  # GET /blogs/new.xml
+  def new
+    @blog = Blog.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @blog }
+    end
+  end
+
+  # GET /blogs/1/edit
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  # POST /blogs
+  # POST /blogs.xml
+  def create
+    @blog = Blog.new(params[:blog])
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to(@blog, :notice => 'Blog was successfully created.') }
+        format.xml  { render :xml => @blog, :status => :created, :location => @blog }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @blog.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /blogs/1
+  # PUT /blogs/1.xml
+  def update
+    @blog = Blog.find(params[:id])
+
+    respond_to do |format|
+      if @blog.update_attributes(params[:blog])
+        format.html { redirect_to(@blog, :notice => 'Blog was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @blog.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.xml
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(blogs_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :blogs
+
   map.resources :feedbacks
   
   # The priority is based upon order of creation: first created -> highest priority.
