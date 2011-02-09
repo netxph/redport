2c0a9bf269dd36560f70330a14cc81c41fef0aba
Marc Vitalis <netxph@sophia.corp.nt.navitaire.com>
2011-02-09 08:36:36 +0800

    cleaned up for rails 3. please use the release-1.0 branch for the older stable version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redport2::Application.config.secret_token = 'ca86f39484e39fe2c491256820c2137a7588cbb16658dcf66aa66ae94e953e0f3be8891e2108c409ce63f7b140be98b74dd10f59ce3bf74c31a4a1cb3e0d2850'
