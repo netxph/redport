6855f5f2470f340532aae2a7387359b78a4272d9
Marc Vitalis <netxph@aisha.(none)>
2012-12-12 13:42:33 +0800

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Redport::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +57,6 @@ Redport::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+  root :to => 'home#index'
 end
