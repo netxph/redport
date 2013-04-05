5c8b507a22bab0410b558207a59c2dbd88076e79
Marc Vitalis <netxph@apple.(none)>
2013-04-06 06:17:59 +0800

    delete all rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Redport::Application.config.secret_token = '7cd732cec91623d8fe496d86c2056abff0720797e6f72ceb37df2d41d20969a55949b88adb63e0491a9eb068a4b87cafa0317bf962f1dcce2f94541d2d039e72'
