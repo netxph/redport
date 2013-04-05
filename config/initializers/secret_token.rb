9eacd61b6c4c6372c08b41491e062e300d42bb20
Marc Vitalis <netxph@apple.(none)>
2013-04-06 06:26:03 +0800

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redport::Application.config.secret_token = '64cf4d70419c697375382a5040684cb2fa0130d23d9850c4e6f4988dfbb234d7f51b75a3277e3792139eeacd51d4eb0532fe231e07603aeb222870ce4b9c9a7c'
