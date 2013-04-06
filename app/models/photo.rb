7c3338547a8b8a268240cb7e4fc82ef99f0f90e6
Marc Vitalis <marc.vitalis@live.com>
2013-04-06 15:11:03 +0800

    added basic photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,11 @@
 class Photo < ActiveRecord::Base
   attr_accessible :category, :description, :name, :url
+
+  def self.get_photos
+    Photo.all
+  end
+
+  def self.get_categories
+    Photo.select(:category).uniq
+  end
 end
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'get_photos should not return null' do
+    photos = Photo.get_photos
+    assert photos != nil
+  end
+
+  test 'get_photos should not be empty' do
+    photos = Photo.get_photos
+    assert photos.count > 0
+  end
+
+  test 'get_categories should not return null' do
+    categories = Photo.get_categories
+    assert categories != nil
+  end
+
+  test 'get_categories should not be empty' do
+    categories = Photo.get_categories
+    assert categories.count > 0
+  end
+
 end
