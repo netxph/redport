3d5b1e49d5f4fa563f2d8547f2fc92d1ffc1ca0b
Marc Vitalis <marc.vitalis@live.com>
2013-04-07 14:23:37 +0800

    converted tests to shoulda and shoulda-context

--- Gemfile
+++ Gemfile
@@ -20,13 +20,15 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-group :tests do
+group :test do
   if RUBY_PLATFORM =~ /(win32|w32)/
     gem 'win32console', '1.3.0'
   end
 
   gem 'minitest'
   gem 'minitest-reporters', '>= 0.5.0'
+  gem 'shoulda'
+  gem 'shoulda-context'
 end
 
 gem 'jquery-rails'
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,69 +1,68 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  test 'model should respond to get_photos' do
-    assert_respond_to Photo, :get_photos
+
+  should validate_presence_of(:name)
+  should validate_presence_of(:category)
+  should validate_presence_of(:url)
+
+  context 'get photos' do
+
+    setup do
+      @photos = Photo.get_photos
+    end
+
+    should 'respond to get_photos' do
+      assert_respond_to Photo, :get_photos
+    end
+
+    should 'not return null' do
+      assert @photos != nil
+    end
+
+    should 'not be empty' do
+      assert_equal 2, @photos.count
+    end
+
   end
 
-  test 'get_photos should not return null' do
-    photos = Photo.get_photos
-    assert photos != nil
+  context 'get categories' do
+
+    setup do
+      @categories = Photo.get_categories
+    end
+
+    should 'respond to get_categories' do
+      assert_respond_to Photo, :get_categories
+    end
+
+    should 'not return null' do
+      assert @categories != nil
+    end
+
+    should 'not be empty' do
+      assert_equal 2, @categories.count
+    end
+
   end
 
-  test 'get_photos should not be empty' do
-    photos = Photo.get_photos
-    assert photos.count > 0
-  end
+  context 'is valid' do
+    setup do
+      @photo = Photo.new
+    end
 
-  test 'model should respond to get_categories' do
-    assert_respond_to Photo, :get_categories
-  end
+    should 'valid if complete' do
+      @photo.name = 'test'
+      @photo.description = 'test description'
+      @photo.category = 'test'
+      @photo.url = 'http://localhost/test.jpg'
 
-  test 'get_categories should not return null' do
-    categories = Photo.get_categories
-    assert categories != nil
-  end
+      assert @photo.valid?
+    end
 
-  test 'get_categories should not be empty' do
-    categories = Photo.get_categories
-    assert categories.count > 0
-  end
-
-  test 'should not be valid if empty' do
-    photo = Photo.new
-
-    assert !photo.valid?
-  end
-
-  test 'should be valid if complete' do
-    photo = Photo.new
-    photo.name = 'test'
-    photo.description = 'test description'
-    photo.category = 'test'
-    photo.url = 'http://localhost/test.jpg'
-
-    assert photo.valid?
-  end
-
-  test 'should not be valid if name is missing' do
-    photo = photos(:flower)
-    photo.name = nil
-
-    assert !photo.valid?
-  end
-
-  test 'should not be valid if category is missing' do
-    photo = photos(:flower)
-    photo.category = nil
-
-    assert !photo.valid?
-  end
-
-  test 'should not be valid if url is missing' do
-    photo = photos(:flower)
-    photo.url = nil
-
-    assert !photo.valid?
+    should 'not valid if empty' do
+      assert !@photo.valid?
+    end
   end
 
 end
